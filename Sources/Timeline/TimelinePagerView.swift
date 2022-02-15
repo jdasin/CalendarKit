79c237bef66ee75e11ae6f573d0f67c9f38be8a7
Daniel Asin <daniel.asin.acha@gmail.com>
2022-02-15 11:44:30 -0400

    Update TimelinePagerView.swift
    Make currentTimeline a public property.

--- Sources/Timeline/TimelinePagerView.swift
+++ Sources/Timeline/TimelinePagerView.swift
@@ -32,7 +32,7 @@ public final class TimelinePagerView: UIView, UIGestureRecognizerDelegate, UIScr
     return offset ?? CGPoint()
   }
   
-  private var currentTimeline: TimelineContainerController? {
+  public var currentTimeline: TimelineContainerController? {
     return pagingViewController.viewControllers?.first as? TimelineContainerController
   }
 
